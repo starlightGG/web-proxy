b93f32b71f44173f15df1a2051273dd9e416f6ab
codingmaster <153460070+starlightGG@users.noreply.github.com>
2026-04-27 17:08:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,7 @@ const WISP_SERVERS = [
   { name: "Stellite Games", url: "wss://stellite.games/wisp/" },
   { name: "Anura Pro", url: "wss://anura.pro/" },
   { name: "Fern Best", url: "wss://fern.best/wisp/" },
-  { name: "Radius Proxy", url: "wss://radiusproxy.app/wisp/" },
-  { name: "t0ast CC", url: "wss://wisp.t0ast.cc/" }
-
+  { name: "Radius Proxy", url: "wss://radiusproxy.app/wisp/" }
 
 ];
 
